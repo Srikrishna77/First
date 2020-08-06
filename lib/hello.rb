ed7098164753c10937f2724d641e27865e67d86d
saikrishna.arige <saikrishna.arige@gmail.com>
2020-08-06 01:20:04 -0700

    Moved grteater.rb to lib

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,11 @@
 puts "Hello, World"
 
+require 'greeter'
+
 puts "Hello, #{ARGV.first}!"
 
 New line fist commit
 Name : sai 
 Email:saikrishna.arige@gmail.com
+greeter = Greeter.new(name)
+puts greeter.greet
